c358f1f3fabef14d935cfffb09463aa8c4b50633
CrystalX127 <65303868+CrystalX127@users.noreply.github.com>
2020-06-05 08:10:09 -0700

    Create Legal_Banner.sh

---
+++ Hardening Scripts/Legal_Banner.sh
@@ -0,0 +1,19 @@
+legal_banner() {
+  # This function adds a legal banner to /etc/motd, /etc/issue and /etc/issue.net
+  echo "
+Unauthorized access to this server is prohibited.
+All connections are monitored and recorded.
+Legal action will be taken. Please disconnect now.
+" > /etc/motd  
+  echo "
+Unauthorized access to this server is prohibited.
+All connections are monitored and recorded.
+Legal action will be taken. Please disconnect now.
+" > /etc/issue
+  echo "
+Unauthorized access to this server is prohibited.
+All connections are monitored and recorded.
+Legal action will be taken. Please disconnect now.
+" > /etc/issue.net
+}
+legal_banner
