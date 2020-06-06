dc68b8dc4db1a5ea958dfea2517c962cb517faeb
CrystalX127 <65303868+CrystalX127@users.noreply.github.com>
2020-06-06 00:18:56 -0400

    Create Disable_Uncommon_Network_Protocols.sh

---
+++ Hardening_Scripts/Disable_Uncommon_Network_Protocols.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+disable_uncommon_network_protocols() 
+{
+  # Disables uncommon network protocols
+  echo "install dccp /bin/true
+install sctp /bin/true
+install tipc /bin/true
+install rds /bin/true" >> /etc/modprobe.d/protocols.conf
+  sudo apt-get --purge remove xinetd nis yp-tools tftpd atftpd tftpd-hpa telnetd rsh-server rsh-redone-server
+}
+disable_uncommon_network_protocols
