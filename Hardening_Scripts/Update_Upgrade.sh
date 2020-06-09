337edc7a6aefcf91f60a5fca88db0624abd4b846
CrystalX127 <65303868+CrystalX127@users.noreply.github.com>
2020-06-09 13:26:40 -0400

    Create Update_Upgrade.sh

---
+++ Hardening_Scripts/Update_Upgrade.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+update_upgrade() {
+  
+  # Updates the package list
+  apt-get update
+  
+  # Upgrades packages
+  apt-get upgrade
+  
+  # Does a dist upgrade, which "inteliggently" handles changing dependencies
+  apt-get dist-upgrade
+}
+update_upgrade
