4a1dd6724f24e69a4d0e9a71ed4a00a5aa56ac34
CrystalX127 <65303868+CrystalX127@users.noreply.github.com>
2020-06-11 10:25:21 -0700

    Update Automatic_Updates.sh

--- Hardening_Scripts/Automatic_Updates.sh
+++ Hardening_Scripts/Automatic_Updates.sh
@@ -2,7 +2,7 @@
 
 automatic_updates() {
   # Enables automatic updates
-  apt install unattended-upgrades 
+  apt-get install unattended-upgrades 
   dpkg-reconfigure -plow unattended-upgrades
 }
 automatic_updates
