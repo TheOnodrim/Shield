87c624d406b8bbe5d667f34284d585f1845879f7
CrystalX127 <65303868+CrystalX127@users.noreply.github.com>
2020-06-11 10:27:16 -0700

    Update Lynis_Recomended_Packages.sh

--- Hardening_Scripts/Lynis_Recomended_Packages.sh
+++ Hardening_Scripts/Lynis_Recomended_Packages.sh
@@ -2,6 +2,6 @@
 
 install_lynis_recommended_packages() {
   # Installs lynis recommended packages
-  apt install apt-listchanges needrestart debsecan debsums libpam-cracklib aide usbguard acct 
+  apt-get install apt-listchanges needrestart debsecan debsums libpam-cracklib aide usbguard acct 
 }
 install_lynis_recommended_packages
