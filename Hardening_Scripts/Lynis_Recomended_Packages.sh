98e845caeca323f8f184142adbc9e69fa0722c36
CrystalX127 <65303868+CrystalX127@users.noreply.github.com>
2020-06-16 11:33:26 -0700

    Delete Lynis_Recomended_Packages.sh

--- Hardening_Scripts/Lynis_Recomended_Packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-install_lynis_recommended_packages() {
-  # Installs lynis recommended packages
-  apt-get install apt-listchanges needrestart debsecan debsums libpam-cracklib aide usbguard acct 
-}
-install_lynis_recommended_packages
