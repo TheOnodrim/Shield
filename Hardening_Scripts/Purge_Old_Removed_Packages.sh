7caf92c7ec6693f1432b504eebd9538d89434a19
CrystalX127 <65303868+CrystalX127@users.noreply.github.com>
2020-06-16 11:33:36 -0700

    Delete Purge_Old_Removed_Packages.sh

--- Hardening_Scripts/Purge_Old_Removed_Packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-purge_old_removed_packages() {
-  # Purges old and removed packages
-  apt autoremove 
-  apt purge "$(dpkg -l | grep '^rc' | awk '{print $2}')" 
-}
-purge_old_removed_packages
