3340eecf7e8e05ec0a3eb3756764398c3cbcd6c7
CrystalX127 <65303868+CrystalX127@users.noreply.github.com>
2020-06-16 11:32:31 -0700

    Delete Disable_Firewire.sh

--- Hardening_Scripts/Disable_Firewire.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-disable_firewire() {
-  # Disables firewire
-  echo "install udf /bin/true
-blacklist firewire-core
-blacklist firewire-ohci
-blacklist firewire-sbp2" >> /etc/modprobe.d/blacklist.conf
-}
-disable_firewire
